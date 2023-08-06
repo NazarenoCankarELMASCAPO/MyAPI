3b568cd40b41a300b40886ef7acf1bbce7814a69
Nazareno cankar <cankarnazareno27@gmail.com>
2023-08-06 01:35:00 -0300

    UPdate fukes

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express")
 const app = express()
-
 const port = 4000
+
+// Conexion a base de datos
+
+const moongose = require("mongoose")
+mongoose.connect('mongodb+srv://umacankar9:umitabb123@myapi-database.2e8w7qc.mongodb.net/?retryWrites=true&w=majority')
+.then(() => console.log("Base de datos conectada"))
+.catch(e => console.log(e))
+
+// 
 
 app.get("/", (req, res) => {
     res.send("WORKS")
