18100e2d067cd8e2a8b965394182c3df282847da
Nazareno cankar <cankarnazareno27@gmail.com>
2023-08-10 11:52:43 -0300

    asd

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Conexion a base de datos
 const mongoose = require("mongoose");
 mongoose
